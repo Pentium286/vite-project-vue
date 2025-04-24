ab3ea325f6582247a32a4dba5661cfe48d101a8b
sheng <sunwhere0207@gmail.com>
2025-04-24 09:32:02 +0800

    1、规整页面。2、添加less

---
+++ src/views/HomeView/index.vue
@@ -0,0 +1,11 @@
+<script setup>
+import TheWelcome from '../../components/TheWelcome.vue';
+</script>
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+<style scoped lang="less">
+@import url("./index.less");
+</style>
