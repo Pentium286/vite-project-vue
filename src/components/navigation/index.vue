4316e6c5f1d942d1261fda914732da891811f81a
sheng <sunwhere0207@gmail.com>
2025-04-24 09:50:27 +0800

    导航抽离

---
+++ src/components/navigation/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+  </nav>
+</template>
+<script>
+import { RouterLink } from 'vue-router';
+</script>
+<style scoped lang="less">
+@import url("./index.less");
+</style>
